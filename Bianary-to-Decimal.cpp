cdadd524f180cb8f3f1a7c0712e0f025497ef391
Muhammad Awais <147119663+Aw4iskh4n@users.noreply.github.com>
2023-10-06 15:12:41 +0500

    Bianary-to-Decimal.cpp

--- Bianary-to-Decimal.cpp
+++ Bianary-to-Decimal.cpp
@@ -5,31 +5,29 @@
 using namespace std;
 
 int main() {
-    
+
     long int bin, i, j, length, dec, result= 0, arr[50];
     cout<<"Enter any Binary: ";
     cin>>bin;
-    
+
     for (i=0; bin > 0 ; i++)
     {
         arr[i] = bin%10;
         bin = bin/10;
-        
-        cout <<endl<<endl<< arr[i]<<endl;
-        
     }
-    
+
     length = i-1;
-    
-    cout<<pow(2,0);
-    
+
+
     for(j = 0 ; j <= length ; j++ )
     {
         dec = arr[j]*(pow(2,j));
         result = result + dec;
-        
-       
+
+
     }
+
+    cout << "The decimal of Binary is : "<<result<<endl;
     
-    cout << "The decimal of Binary is : "<<result;
+    main();
 }
